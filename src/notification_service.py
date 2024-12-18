58f102f00d9e0e9ad444a3a399a2a156c99c4863
tazzledazzle <terenceschumacher@gmail.com>
2024-12-18 13:57:57 -0800

    updated pylint files

--- src/notification_service.py
+++ src/notification_service.py
@@ -1,11 +1,19 @@
+"""
+    Notification Service
+"""
+import typing
 from fastapi import FastAPI, WebSocket
-from typing import List
 
 app = FastAPI()
-connected_clients: List[WebSocket] = []
+connected_clients: typing.List[WebSocket] = []
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
+    """
+    real-time tasks
+    :param websocket:
+    :return:
+    """
     await websocket.accept()
     connected_clients.append(websocket)
     try:
@@ -13,5 +21,8 @@ async def websocket_endpoint(websocket: WebSocket):
             data = await websocket.receive_text()
             for client in connected_clients:
                 await client.send_text(data)
-    except:
+    except RuntimeError as e:
         connected_clients.remove(websocket)
+        print(f"{e}")
+    finally:
+        pass
--- src/user_service.py
+++ src/user_service.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI, HTTPException, Depends
-from pydantic import BaseModel
-from jose import jwt
+"""
+    User Service
+"""
 from datetime import datetime, timedelta
+# pylint: disable=syntax-error
+from jose import jwt
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -10,10 +14,21 @@ users = {"admin": {"password": "admin"}}
 SECRET_KEY = "mysecretkey"
 
 class User(BaseModel):
+    """
+    User(BaseModel)
+    :param username
+    :param password
+    """
     username: str
     password: str
 
 def authenticate_user(username: str, password: str):
+    """
+    authenticate_user
+    :param username:
+    :param password:
+    :return:
+    """
     user = users.get(username)
     if not user or user["password"] != password:
         return False
@@ -21,7 +36,13 @@ def authenticate_user(username: str, password: str):
 
 @app.post("/login/")
 def login(user: User):
+    """
+    /login/
+    :param user:
+    :return:
+    """
     if not authenticate_user(user.username, user.password):
         raise HTTPException(status_code=401, detail="Invalid credentials")
-    token = jwt.encode({"sub": user.username, "exp": datetime.utcnow() + timedelta(hours=1)}, SECRET_KEY)
+    token = jwt.encode({"sub": user.username, "exp": datetime.now() + timedelta(hours=1)},
+                       SECRET_KEY)
     return {"access_token": token}
